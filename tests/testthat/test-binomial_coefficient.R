3330734b6cbfca9f901dce21bf3f8eb4fe68d67f
murielburi <muriellynnea.buri@uzh.ch>
2018-01-24 15:20:55 +0100

    Vectorize binomial_coefficient
    This closes #1

--- tests/testthat/test-binomial_coefficient.R
+++ tests/testthat/test-binomial_coefficient.R
@@ -6,6 +6,9 @@ test_that("valid input", {
   expect_equal(3003, binomial_coefficient(15, 5))
   expect_equal(1, binomial_coefficient(15, 0))
   expect_equal(1, binomial_coefficient(15, 15))
+  expect_equal(c(1, 3, 6), binomial_coefficient(2:4, 2))
+  expect_equal(c(1, 3, 3, 1), binomial_coefficient(3, 0:3))
+  expect_equal(c(1, 3, 15, 84), binomial_coefficient(3 * 0:3, 0:3))
 })
 
 test_that("invalid input", {
@@ -14,6 +17,4 @@ test_that("invalid input", {
   expect_error(binomial_coefficient(2, 1.1))
   expect_error(binomial_coefficient(1.1, 1))
   expect_error(binomial_coefficient(1, 2))
-  expect_error(binomial_coefficient(1:2, 0))
-  expect_error(binomial_coefficient(2, 0:2))
 })
