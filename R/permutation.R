b500c4f792b01e68e3802d8e49d51c895342d189
wiep <wiep@users.noreply.github.com>
2018-02-01 09:32:19 +0100

    Add function permute_vector

---
+++ R/permutation.R
@@ -0,0 +1,24 @@
+permute_vector <- function(x) {
+  n <- length(x)
+  num_permutation <- factorial(n)
+
+  if (n == 1) { # base case
+    return(as.matrix(x))
+  } else {
+    num_sub_permutation <- factorial(n - 1)
+    res <- matrix(0.0, nrow = n, ncol = num_permutation)
+    for (i in 1:n) {
+      y <- x
+      # swap first entry in y with i-th
+      tmp <- y[1]
+      y[1] <- y[i]
+      y[i] <- tmp
+
+      # calculate all permutations of y[2:n]
+      col_idx <- (i-1) * num_sub_permutation + seq(1:num_sub_permutation)
+      res[1, col_idx] <- y[1]
+      res[2:n, col_idx] <- permute_vector(y[2:n])
+    }
+    return(res)
+  }
+}
