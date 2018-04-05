87284dff5ce5cca4b85475b4ec92e8aacad6e2d1
Johannes Brachem <jobrachem@posteo.de>
2018-04-05 15:50:53 +0200

    remove example

--- R/multiples_of_3_and_5.R
+++ R/multiples_of_3_and_5.R
@@ -2,9 +2,7 @@
 #'
 #' @param x A natural number.
 #' @return The sum of all natural numbers below \code{x} that are multiples of 3 or 5.
-#' @examples
-#' multiples(10)
-#' multiples(-10)
+
 
 multiples <- function(x) {
   if (!is.numeric(x)) stop("x needs to be numeric")
